26457542f433792e5af19bb8ccc1a09a246c1ced
Sergey Poliakov <ps.force@gmail.com>
2016-05-13 13:42:41 +0300

    Viewport added

--- js/current.js
+++ js/current.js
@@ -32,7 +32,7 @@ var saint = {
     uber: 300,
     radissonRoyalBlueBusiness: 8300,
     hostel: 500,
-    palkinbigdinner: 8400,
+    palkinbigdinner: 8400
 };
 
 saintArr = Object.keys(saint);
@@ -60,7 +60,7 @@ $(function () {
             }
         }
     ).done(function () {
-        //var blya = parseFloat((fx.convert(saint.metro)).toFixed(2));
+        //var something = parseFloat((fx.convert(saint.metro)).toFixed(2));
         //var demo = new CountUp('one', 0, 0, 2, 0.7, options);
         //demo.update(blya);
         //demo.start();
@@ -83,10 +83,10 @@ $(function () {
             for (i in saintArr) {
                 var saintArrPos = saintArr[i];
                 if (anchorLink == saintArrPos && saintArrPos != 'welcome') {
-                    var blya = parseFloat((fx.convert(saint[saintArrPos]).toFixed(2)));
+                    var convertMoney = parseFloat((fx.convert(saint[saintArrPos]).toFixed(2)));
                     var tagname = 's' + saintArrPos;
                     //console.log(tagname);
-                    var myCounter = new CountUp(tagname, 0, blya, 2, 0.7, options);
+                    var myCounter = new CountUp(tagname, 0, convertMoney, 2, 0.7, options);
                     myCounter.start();
                 }
             }
